117afc9da41662c55fbacf3acc2100f1d6ec11a6
larkguo <larkguo@gmail.com>
2020-04-06 18:00:12 +0800

    Update gluster-dht.c

--- DHT/gluster-dht.c
+++ DHT/gluster-dht.c
@@ -10,80 +10,13 @@
 #include <stdbool.h>
 #include <regex.h>
 
-uint32_t SuperFastHash (const char * data, int32_t len);
-uint32_t gf_dm_hashfn (const char *msg, int len);
-uint32_t ReallySimpleHash (char *path, int len);
-
-#define get16bits(d) (*((const uint16_t *) (d)))
 #define DM_DELTA 0x9E3779B9
 #define DM_FULLROUNDS 10        /* 32 is overkill, 16 is strong crypto */
 #define DM_PARTROUNDS 6         /* 6 gets complete mixing */
 
-uint32_t
-ReallySimpleHash (char *path, int len)
-{
-        uint32_t        hash = 0;
-        for (;len > 0; len--)
-                hash ^= (char)path[len];
-
-        return hash;
-}
-
-/*
-  This is apparently the "fastest hash function for strings".
-  Written by Paul Hsieh <http://www.azillionmonkeys.com/qed/hash.html>
-*/
-/* In any case make sure, you return 1 */
-uint32_t SuperFastHash (const char * data, int32_t len) {
-        uint32_t hash = len, tmp;
-        int32_t rem;
-
-        if (len <= 1 || data == NULL) return 1;
-
-        rem = len & 3;
-        len >>= 2;
-
-        /* Main loop */
-        for (;len > 0; len--) {
-                hash  += get16bits (data);
-                tmp    = (get16bits (data+2) << 11) ^ hash;
-                hash   = (hash << 16) ^ tmp;
-                data  += 2*sizeof (uint16_t);
-                hash  += hash >> 11;
-        }
-
-        /* Handle end cases */
-        switch (rem) {
-        case 3: hash += get16bits (data);
-                hash ^= hash << 16;
-                hash ^= data[sizeof (uint16_t)] << 18;
-                hash += hash >> 11;
-                break;
-        case 2: hash += get16bits (data);
-                hash ^= hash << 11;
-                hash += hash >> 17;
-                break;
-        case 1: hash += *data;
-                hash ^= hash << 10;
-                hash += hash >> 1;
-        }
-
-        /* Force "avalanching" of final 127 bits */
-        hash ^= hash << 3;
-        hash += hash >> 5;
-        hash ^= hash << 4;
-        hash += hash >> 17;
-        hash ^= hash << 25;
-        hash += hash >> 6;
-
-        return hash;
-}
-
 /* Davies-Meyer hashing function implementation
  */
-static int
-dm_round (int rounds, uint32_t *array, uint32_t *h0, uint32_t *h1)
-{
+static int dm_round (int rounds, uint32_t *array, uint32_t *h0, uint32_t *h1){
         uint32_t sum = 0;
         int      n = 0;
         uint32_t b0  = 0;
@@ -110,9 +43,7 @@ dm_round (int rounds, uint32_t *array, uint32_t *h0, uint32_t *h1)
         return 0;
 }
 
-uint32_t
-__pad (int len)
-{
+uint32_t __pad (int len){
         uint32_t pad = 0;
 
         pad = (uint32_t) len | ((uint32_t) len << 8);
@@ -121,9 +52,7 @@ __pad (int len)
         return pad;
 }
 
-uint32_t
-gf_dm_hashfn (const char *msg, int len)
-{
+uint32_t gf_dm_hashfn (const char *msg, int len){
         uint32_t  h0 = 0x9464a485;
         uint32_t  h1 = 0x542e1a94;
         uint32_t  array[4];
@@ -135,7 +64,6 @@ gf_dm_hashfn (const char *msg, int len)
         int       full_bytes = 0;
         uint32_t *intmsg = NULL;
         int       word = 0;
-
 
         intmsg = (uint32_t *) msg;
         pad = __pad (len);
@@ -176,9 +104,7 @@ gf_dm_hashfn (const char *msg, int len)
         return h0 ^ h1;
 }
 
-static bool 
-dht_munge_name (const char *original, char *modified,size_t len, regex_t *re)
-{
+static bool dht_munge_name (const char *original, char *modified,size_t len, regex_t *re){
         regmatch_t  matches[2] = {{0}, };
         size_t      new_len    = 0;
         int         ret        = 0;
@@ -209,9 +135,7 @@ using regex '^\.(.+)\.[^.]+$'
 dht_munge_name '.fio.sh' -> 'fio'
 gf_dm_hashfn('fio')	= 2164401422	(0x8102250e)
 */
-void 
-main(int argc, char ** argv)
-{
+void main(int argc, char ** argv){
     int i = 0;
     char  *name = NULL;
     char  *rsync_friendly_name = NULL;
